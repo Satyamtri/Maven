c07b75cd47cd40585c784d68ffb049b2c2567c1e
Tripathi <satyam.tripathi@mind.motherson.com>
2022-06-14 11:08:40 +0530

    Hello

---
+++ src/main/java/Maven/Hello/App.java
@@ -0,0 +1,13 @@
+package Maven.Hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Satyam Tripathi" );
+    }
+}
